75ed1e8f90e11daa8628f6d1a5c439990b19c7a2
Afsalnitk <127192860+Afsalnitk@users.noreply.github.com>
2023-05-08 12:51:35 +0530

    Update Source_Code_Deflection_Profile.m

--- Source_Code_Deflection_Profile.m
+++ Source_Code_Deflection_Profile.m
@@ -1,7 +1,7 @@
 clc
 clear all
 
-%-------------Defection Profile-----------------------------------------------------------
+%-------------Defection Profile----------------------------------------------------------- 
  BCs=menu('Boundary Conditions : ','CCCC','SCSC','SSSC','SSSS','SFSC','SFSS','CFCF','SFSF');
  [b1,IW1_1,IW1_2,IW1_3,IW1_4,IW1_5,IW1_6,IW1_7,IW1_8,b2,LW2_1,b3,LW3_2,b4,LW4_3,b5,LW5_4] = Parameters(BCs);
 
